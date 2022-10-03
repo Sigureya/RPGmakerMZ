dc530fcc8068d049001dfdc6644c052460bc71a9
Sigureya <sigureya1208@gmail.com>
2022-10-04 06:47:08 +0900

    スキルタイプの確認を追加

--- Mano_DisableActorCommand_IfNoSkill_Is_Available.js
+++ Mano_DisableActorCommand_IfNoSkill_Is_Available.js
@@ -3,6 +3,12 @@
  * @author しぐれん(https://github.com/Sigureya/RPGmakerMV)
  * 
  * @target MZ
+ * 
+ * @param skillTypeList
+ * @text 条件チェックを行うスキルタイプ
+ * @type number[]
+ * @default []
+ * 
  * @help
  * 戦闘中のアクターコマンドに関する機能です。
  * コマンドに対応した使用可能なスキルが無い場合、コマンドを選択不能にします。
@@ -11,9 +17,29 @@
 */
 
 (function(){
+    /**
+     * @type {String}
+     */
+    const  PLUGIN_NAME= ('Mano_DisableActorCommand_IfNoSkill_Is_Available');
+    function getParam(){ return PluginManager.parameters(PLUGIN_NAME);  }
+    
+const setting = (function(){
+
+    const param =getParam();
+    /**
+     * @type {string[]}
+     */
+    const skillTypeList = JSON.parse(param.skillTypeList||"[]");
+    const skillTypeSet = new Set(skillTypeList.map(Number));
+    const result ={
+        skillTypes:skillTypeSet,
+    };
+    return result;
+})();
+
 const Window_ActorCommand_addCommand=Window_ActorCommand.prototype.addCommand;
 Window_ActorCommand.prototype.addCommand =function(name,symbol,enabled,ext){
-    if(symbol ==="skill" && ext){
+    if( enabled && symbol ==="skill" && setting.skillTypes.has(ext)){
         const en2= this._actor.skills().some( (skill)=>{
             return skill.stypeId === ext && this._actor.canUse(skill);
         })
